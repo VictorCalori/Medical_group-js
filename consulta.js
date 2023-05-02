9361feb28f01f8f6ea4b92cf07fb3ca7608253ac
Calorithefacto <125594287+VictorCalori@users.noreply.github.com>
2023-05-02 13:45:20 -0300

    Add files via upload

---
+++ consulta.js
@@ -0,0 +1,3 @@
+function redirectinicio(){
+    window.location.assign("http://127.0.0.1:5500/Tela-de-inicio-desktop/Tela-de-%C3%ADnicio-desktop.html")
+}
---
+++ localização.js
@@ -0,0 +1,3 @@
+function redirectonde(){
+    window.location.assign("http://127.0.0.1:5500/Tela-de-inicio-desktop/Tela-de-%C3%ADnicio-desktop.html")
+}
